b4a53f63bb5d6b97c883078ddff406bee757c00c
Anders Fredrik Kiær <akia@statoil.com>
2018-11-03 22:13:33 +0100

    Fix header element

--- visualizations/petech_page_element/webviz_petech_page_element/__init__.py
+++ visualizations/petech_page_element/webviz_petech_page_element/__init__.py
@@ -22,4 +22,11 @@ class PetechPageElement(JSONPageElement):
         pass
 
     def _repr_html_(self):
-        return self.get_template().render(element=self, root_folder='.')
+        html = ""
+
+        for elem in self.header_elements:
+            if not any(key in ["href", "src"] for key, _ in elem.attributes):
+                html += str(elem)
+                html += "\n"
+
+        return html + self.get_template().render(element=self, root_folder='.')
